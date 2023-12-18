753496f69fd2d9d52adaedb01513d8c89f0e9aa4
Vladimir <146058049+Vladimir210690@users.noreply.github.com>
2023-12-18 13:58:35 +0300

    Template Function

---
+++ Shablon_foo.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+template <typename ki>
+void Foo(ki &x, ki &y) {
+	ki g = x;
+	x = y;
+	y = g;
+}
+
+int main() {
+	setlocale(LC_ALL, "RU");
+
+	string a = "Vova";
+	string b = "Kolia";
+	double c = 76.1;
+	double d = 4;
+
+	cout << a << "\t" << b << endl;
+	cout << c << "\t" << d << endl;
+	cout << endl;
+	Foo(a, b);
+	Foo(c, d);
+	cout << a << "\t" << b << endl;
+	cout << c << "\t" << d << endl;
+	return 0;
+}
