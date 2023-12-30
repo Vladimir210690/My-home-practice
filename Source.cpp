2202f52f094a9346745903b801c5c1448e859934
Vladimir <146058049+Vladimir210690@users.noreply.github.com>
2023-12-30 11:56:25 +0300

    Работа с динамической памятью
    Доводил её до ошибки

--- Source.cpp
+++ Source.cpp
@@ -1,13 +1,27 @@
 #include <iostream>
+#include <ctime>
 
 using namespace std;
 
 int main() {
-	setlocale(LC_ALL, "ru");
+	setlocale(LC_ALL, "RU");
+	srand(time(NULL));
+	long long size = 0;
+	cout << "Сколько выделить памяти?: ";
+	cin >> size;
 
-	for (int i = 0; i < 256; i++) {
-		cout << "\tКод: " << i << " \tСимвол: " << (char)i << endl;
+	int* pntr = new int[size];
+
+	for (int i = 0; i < size; i++) {
+		*(pntr + i) = rand() % 100 + 1;
+		cout << pntr + i << "\t" << *(pntr + i) << endl;
 	}
+
+	system("pause");
+	delete[]pntr;
+
+
+
 
 	return 0;
 }
