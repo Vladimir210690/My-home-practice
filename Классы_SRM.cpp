c311a2829e66622fbf81331847fe9c1cbf2b6b03
Vladimir <146058049+Vladimir210690@users.noreply.github.com>
2024-01-07 16:11:05 +0300

    Print SRM class

---
+++ Классы_SRM.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class People {
+public:
+	int age;
+	int weight;
+	int hight;
+	string name;
+
+	void Input() {
+		cout << "Input you data:" << endl;
+		cout << "Input name: ";
+		cin >> name;
+		cout << "Input age: ";
+		cin >> age;
+		cout << "Input weight: ";
+		cin >> weight;
+		cout << "Input hight: ";
+		cin >> hight;
+		cout << endl;
+		system("cls");
+	}
+	void Print() {
+		cout << "Name: " << name << " Age: " << age << " Weight: " << weight << " Hight: " << hight << endl;
+		cout << "----------------------------------------------------------------------" << endl;
+	}
+};
+
+
+int main() {
+
+	People First;
+	People Second;
+	People Third;
+
+	First.Input();
+	Second.Input();
+	Third.Input();
+
+	First.Print();
+	Second.Print();
+	Third.Print();
+
+	return 0;
+}
