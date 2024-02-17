bca951e2d9f84568c94afe980b0c2f219c48cc8b
Vladimir <146058049+Vladimir210690@users.noreply.github.com>
2024-02-17 20:45:51 +0300

    Многофайловый проект

---
+++ General.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Header.h"
+
+using namespace std;
+
+int main() {
+
+	cout << sum(8, 23) << endl;
+	cout << sum(5, 11, 16) << endl;
+
+	return 0;
+}
---
+++ sum.cpp
@@ -0,0 +1,11 @@
+#include "Header.h"
+
+int sum(int a, int b) {
+	int c = a + b;
+	return c;
+}
+
+int sum(int a, int b, int d) {
+	int c = a + b + d;
+	return c;
+}
