fb6784326182228c5b5eafffe051ca9360e26c5d
Vladimir <146058049+Vladimir210690@users.noreply.github.com>
2024-03-20 16:39:11 +0300

    using getline

---
+++ Строки с пробелами.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	string fio;
+	cout << "Enter you fio: ";
+	getline(cin, fio);
+	cout << endl << endl;
+	cout << " You fio: " << fio << endl;
+
+	return 0;
+}
